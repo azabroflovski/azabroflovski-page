539dbd2d771995ae4e2a32011291e59cc69f7db7
azabroflovski <azabroflovski@gmail.com>
2021-09-18 22:59:32 +0500

    feat(404): add page not found

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
    build: {
-      outDir: 'build'
+      outDir: 'build',
+      rollupOptions: {
+         input: {
+            main: '/index.html',
+            pageNotFound: '/404.html',
+         }
+      },
    }
 })
